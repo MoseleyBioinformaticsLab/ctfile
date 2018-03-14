88ff7171aebda4ac151e5c31d4ecd7b42634aeda
Andrey Smelter <andrey.smelter@gmail.com>
2018-03-13 23:53:53 -0400

    Added utils helper functions module.

---
+++ ctfile/utils.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+from collections import Counter
+
+class OrderedCounter(Counter, OrderedDict):
+    """Counter that remembers the order of elements are first seen."""
+
+    def __repr__(self):
+        return "{}({})".format(self.__class__.__name__, OrderedDict(self))
+
+    def __reduce__(self):
+        return self.__class__, OrderedDict(self)
