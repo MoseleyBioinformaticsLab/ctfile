a827aca5b861dd6f8bbcd5a6bcbe23f9f853d636
Andrey Smelter <andrey.smelter@gmail.com>
2018-03-15 23:48:36 -0400

    Added 'Ctab' configuration file import.

--- ctfile/utils.py
+++ ctfile/utils.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 
+import os
+import json
 from collections import OrderedDict
 from collections import Counter
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+ctab_properties_path = '{}/conf/Ctab_properties.json'.format(this_directory)
+with open(ctab_properties_path, 'r') as infile:
+    ctab_properties_conf = json.load(infile)
+
 
 class OrderedCounter(Counter, OrderedDict):
     """Counter that remembers the order of elements are first seen."""
