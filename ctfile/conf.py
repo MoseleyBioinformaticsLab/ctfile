09e173634cbd2fa893b05937adca570e694f8c2c
Andrey Smelter <andrey.smelter@gmail.com>
2018-03-29 23:45:38 -0400

    Line break.

--- ctfile/conf.py
+++ ctfile/conf.py
@@ -7,5 +7,6 @@ import json
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 ctab_properties_path = '{}/config_files/Ctab_properties.json'.format(this_directory)
+
 with open(ctab_properties_path, 'r') as infile:
     ctab_properties_conf = json.load(infile)
