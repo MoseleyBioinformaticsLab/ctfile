a5347fc04ea6c4f68ca5a812b90d8b131cb77c9e
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-03 13:44:42 -0400

    Added "api" functions to top-level of "ctfile" package.

--- ctfile/__init__.py
+++ ctfile/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .fileio import read_file
-
+from .api import load
+from .api import loadstr
+from .api import read_file
+from .api import read_files
 
 __version__ = '0.1.0'
