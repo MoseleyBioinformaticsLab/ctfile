45781d7905254f9a5b8d2cd866a19d8aed65cb9b
Andrey Smelter <andrey.smelter@gmail.com>
2018-04-16 14:51:34 -0400

    Added default logger.

--- ctfile/__init__.py
+++ ctfile/__init__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
+
 from .api import load
 from .api import loadstr
 from .api import read_file
 from .api import read_files
 
 __version__ = '0.1.0'
+
+
+try:  # Python 2/3 compatibility code
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+
+# Setting default logging handler
+logging.getLogger(__name__).addHandler(NullHandler())
