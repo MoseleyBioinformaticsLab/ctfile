4310b25466aa2dd3d562814203d4cd6f07fdaa72
Andrey Smelter <andrey.smelter@gmail.com>
2018-08-15 00:12:03 -0400

    utils module docstring.

--- ctfile/utils.py
+++ ctfile/utils.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 
+"""
+ctfile.utils
+~~~~~~~~~~~~
+
+This module provides reusable utility functions and objects.
+"""
+
 from collections import OrderedDict
 from collections import Counter
 
 
 class OrderedCounter(Counter, OrderedDict):
-    """Counter that remembers the order of elements are first seen."""
+    """Counter that remembers the order of elements that are seen first."""
 
     def __repr__(self):
         return "{}({})".format(self.__class__.__name__, OrderedDict(self))
