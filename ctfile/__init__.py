24cddecccaaa25f75b9edfa6066b92a1dea4a831
Andrey Smelter <andrey.smelter@gmail.com>
2018-08-14 23:22:30 -0400

    Added package-level docstring.

--- ctfile/__init__.py
+++ ctfile/__init__.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""The ``ctfile`` package is a Python library that facilitates 
+reading and writing of ``CTfile`` formats. CTfile stands for 
+Chemical Table file which is a family of text-based chemical 
+file formats and is used to describe chemical molecules and 
+reactions.
+
+This package includes the following modules:
+
+``api``
+    This modules provides core API functions and objects.
+   
+``conf``
+    This module reads in configuration files.
+
+``ctfile``
+    This module implements core objects to represent ``CTfile`` file format.
+   
+``tokenizer``
+    This module implements tokenizer that processes text file and yields 
+    tokens necessary to create ``CTfile`` objects.
+
+``utils``
+    This module provides reusable utility functions and objects.
+"""
+
+
 import logging
 
 from .api import load
