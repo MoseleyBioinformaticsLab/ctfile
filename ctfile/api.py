2727ddfc3d82960e5171a23b62d74a46d060f112
andreysmelter <andrey.smelter@gmail.com>
2019-07-19 16:10:45 -0400

    Removed unnecessary API imports.

--- ctfile/api.py
+++ ctfile/api.py
@@ -10,14 +10,6 @@ This module provides core API functions and objects.
 
 from filehandles import filehandles
 from .ctfile import CTfile
-from .ctfile import Ctab
-from .ctfile import Molfile
-from .ctfile import SDfile
-from .ctfile import Atom
-from .ctfile import Bond
-
-load = CTfile.load
-loadstr = CTfile.loadstr
 
 
 def read_file(path, verbose=False):
